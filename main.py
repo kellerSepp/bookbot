20d0683193c544bb223c6421af6b2a8562f2c323
kellerSepp <se.kellermann@gmail.com>
2025-09-22 22:50:54 +0200

    implemented sys.args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import count_characters
 from stats import sort_dictionary_as_list
@@ -9,19 +10,22 @@ def get_book_text(filepath):
     return book_as_string
 
 def main():
-    #print("foo")
-    book_path = "books/frankenstein.txt"
-    book_string = get_book_text(book_path)
-    counted_words = count_words(book_string)
-    counted_characters = count_characters(book_string)
-    #print(counted_characters)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}")
-    print("----------- Word Count ----------")
-    print(f"Found {counted_words} total words")
-    print("--------- Character Count -------")
-    character_count = sort_dictionary_as_list(counted_characters)
-    for char_count in character_count:
-        print(f"{char_count["char"]}: {char_count["num"]}")
-    print("============= END ===============")
+    try:
+        script_name = sys.argv[0]
+        book_path = sys.argv[1]
+        book_string = get_book_text(book_path)
+        counted_words = count_words(book_string)
+        counted_characters = count_characters(book_string)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}")
+        print("----------- Word Count ----------")
+        print(f"Found {counted_words} total words")
+        print("--------- Character Count -------")
+        character_count = sort_dictionary_as_list(counted_characters)
+        for char_count in character_count:
+            print(f"{char_count["char"]}: {char_count["num"]}")
+        print("============= END ===============")
+    except Exception as e:
+        print("Usage: python3 main.py <path_to_book>")
+        return sys.exit(1)
 main()
